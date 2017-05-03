4605d55045f034618a86d31bfc45495e81861572
Diego Teliz <diegoteliz007@gmail.com>
2017-05-03 12:35:24 +1200

    Add minor code improvement

--- miscellaneous/password-protect/info.php
+++ miscellaneous/password-protect/info.php
@@ -1,6 +1,6 @@
 <?php
     // Get full path to .htpasswd file
     $dir = dirname(__FILE__);
-    echo '<p>Full path to this dir: <b>' . $dir . '</b></p>';
-    echo '<p>Full path to a .htpasswd file in this dir: <b>' . $dir . '/.htpasswd' . '</b></p>';
+    echo '<p>Full path to this dir: <b>'.$dir.'</b></p>';
+    echo '<p>Full path to a .htpasswd file in this dir: <b>'.$dir.'/.htpasswd</b></p>';
 ?>
